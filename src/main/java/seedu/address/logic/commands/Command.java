079bf213f2ce2ad72c32cd4471337b1de745fad7
Stuart Lim <61983502+press-stuart@users.noreply.github.com>
2026-04-02 20:29:08 +0800

    Add new signature for Command#execute

--- src/main/java/seedu/address/logic/commands/Command.java
+++ src/main/java/seedu/address/logic/commands/Command.java
@@ -1,5 +1,6 @@
 package seedu.address.logic.commands;
 
+import seedu.address.logic.PersonListView;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
@@ -12,9 +13,22 @@ public abstract class Command {
      * Executes the command and returns the result message.
      *
      * @param model {@code Model} which the command should operate on.
+     * @param personListView List which is viewed by the user before command execution.
      * @return feedback message of the operation result for display
      * @throws CommandException If an error occurs during command execution.
      */
-    public abstract CommandResult execute(Model model) throws CommandException;
+    public abstract CommandResult execute(Model model, PersonListView personListView) throws CommandException;
+
+    /**
+     * Executes the command and returns the result message.
+     *
+     * @param model {@code Model} which the command should operate on.
+     * @return feedback message of the operation result for display
+     * @throws CommandException If an error occurs during command execution.
+     * @deprecated Use {@link #execute(Model, PersonListView)} instead, which specifies the currently viewed list.
+     */
+    @Deprecated public CommandResult execute(Model model) throws CommandException {
+        return execute(model, PersonListView.KEPT_PERSONS);
+    }
 
 }
--- src/main/java/seedu/address/logic/commands/ExportCommand.java
+++ src/main/java/seedu/address/logic/commands/ExportCommand.java
@@ -7,6 +7,7 @@ import java.nio.file.Path;
 import java.util.List;
 
 import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.PersonListView;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.csv.CsvWriterUtil;
 import seedu.address.model.Model;
@@ -40,7 +41,7 @@ public class ExportCommand extends Command {
     }
 
     @Override
-    public CommandResult execute(Model model) throws CommandException {
+    public CommandResult execute(Model model, PersonListView personListView) throws CommandException {
         requireNonNull(model);
 
         List<Person> persons = model.getAddressBook().getKeptPersonList();
